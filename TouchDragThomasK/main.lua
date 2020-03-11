53fe44b64f608ac955383f8b1e96fcbe97e5d6dc
Thomas Kushner <61517003+s276992@users.noreply.github.com>
2020-03-11 19:13:46 -0400

    Multiple images are moving

--- TouchDragThomasK/main.lua
+++ TouchDragThomasK/main.lua
@@ -1,7 +1,18 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: TouchAndDrag
+-- Name: Thomas Kushner
+-- Course: ICS2O
+-- This programm has 2 images that can be dragged around
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- set the background colour
+display.setDefault ("background", 0/255, 100/255, 0/255)
+
+local waluigi = display.newImageRect ("Images/t-pose.png", 200, 200)
+local filthyFrank = display.newImageRect ("Images/franku.png", 200, 200)
+local waluigiWidth = waluigi.waluigiWidth
+local waluigiHeight = waluigi.waluigiHeight
+local frankWidth = frank.frankWidth
+local frankHeight = frank.frankHeight
