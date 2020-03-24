31d18ce5877adcf7fb1fa35366d7ac054bfab371
Thomas Kushner <61517003+s276992@users.noreply.github.com>
2020-03-24 11:44:27 -0400

    Drags objects around the screen

--- TouchDragThomasK/main.lua
+++ TouchDragThomasK/main.lua
@@ -7,12 +7,109 @@
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
--- set the background colour
-display.setDefault ("background", 0/255, 100/255, 0/255)
+-- create local variables
+local backgroundImage = display.newImageRect("Images/sky.png", 2048, 1536)
+local Tyler = display.newImageRect("Images/Tyler.png", 250, 150)
+local tylerHeight = Tyler.height
+local tylerWidth = Tyler.width 
 
-local waluigi = display.newImageRect ("Images/t-pose.png", 200, 200)
-local filthyFrank = display.newImageRect ("Images/franku.png", 200, 200)
-local waluigiWidth = waluigi.waluigiWidth
-local waluigiHeight = waluigi.waluigiHeight
-local frankWidth = frank.frankWidth
-local frankHeight = frank.frankHeight
+local Igor = display.newImageRect("Images/IGOR.png", 250, 150)
+local igorHeight = Igor.height
+local igorWidth = Igor.width 
+
+local Krabs = display.newImageRect("Images/Krabs.png", 250, 150)
+local krabsHeight = Krabs.height
+local krabsWidth = Krabs.width
+
+-- my boolean variables are to keep track of which object I touch first
+local alreadyTouchedTyler = false
+local alreadyTouchedIgor = false
+local alreadyTouchesKrabs = false
+
+-- set the initial (x,y) position of the objects
+Tyler.x = 300
+Tyler.y = 200
+
+Igor.x = 400
+Igor.y = 350
+
+Krabs.x = 650
+Krabs.y = 500
+
+-- Function: TylerListener
+-- Input: touch listener
+-- Output: none
+-- Description: when tyler is touched, move him
+local function TylerListener(touch)
+	if (touch.phase == "began") then
+		if (alreadyTouchedIgor == false) and (alreadyTouchesKrabs== false) then
+			alreadyTouchedTyler = true
+		end	
+    end 
+
+    if ( (touch.phase == "moved") and (alreadyTouchedTyler == true) ) then
+	   Tyler.x = touch.x 
+	   Tyler.y = touch.y
+    end 
+
+    if (touch.phase == "ended") then
+    	alreadyTouchedTyler = false
+    	alreadyTouchedIgor = false
+    	alreadyTouchesKrabs = false
+    end
+end
+
+-- add the listeners to each object
+Tyler: addEventListener("touch", TylerListener)
+
+-- Function:IgorListener
+-- Input: touch listener
+-- Output: none
+-- Description: when igor is touched, move him
+local function IgorListener(touch)
+	if (touch.phase == "began") then
+		if (alreadyTouchedTyler == false) and (alreadyTouchesKrabs== false) then
+			alreadyTouchedIgor = true
+		end	
+    end 
+
+    if ( (touch.phase == "moved") and (alreadyTouchedIgor == true) ) then
+	   Igor.x = touch.x 
+	   Igor.y = touch.y
+    end 
+
+    if (touch.phase == "ended") then
+    	alreadyTouchedTyler = false
+    	alreadyTouchedIgor = false
+    	alreadyTouchesKrabs = false
+    end
+end
+
+-- add the listeners to each object
+Igor: addEventListener("touch", IgorListener)
+
+-- Function:KrabsListener
+-- Input: touch listener
+-- Output: none
+-- Description: when krabs is touched, move him
+local function KrabsListener(touch)
+	if (touch.phase == "began") then
+		if (alreadyTouchedTyler == false) and (alreadyTouchesIgor== false) then
+			alreadyTouchedKrabs = true
+		end	
+    end 
+
+    if ( (touch.phase == "moved") and (alreadyTouchedKrabs == true) ) then
+	   Krabs.x = touch.x 
+	   Krabs.y = touch.y
+    end 
+
+    if (touch.phase == "ended") then
+    	alreadyTouchedTyler = false
+    	alreadyTouchedIgor = false
+    	alreadyTouchedKrabs = false
+    end
+end
+
+-- add the listeners to each object
+Krabs: addEventListener("touch", KrabsListener)
